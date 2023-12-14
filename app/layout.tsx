8102449e5f42828d51b9bdc8d87101570a325b02
Zain Ali <ZainSoftwear11@gmail.com>
2023-12-14 16:13:39 +0500

    Changed font from inter to poppins

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import "@styles/globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Poppins({ weight: "400", subsets: ["latin"] });
 export const metadata: Metadata = {
 	title: "Github",
 	description: "Github Landing Page Clone",
