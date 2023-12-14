6f0c3fb00b14324814beea39fd63dc7b9043c9da
Zain Ali <ZainSoftwear11@gmail.com>
2023-12-14 16:16:12 +0500

    Exported Images and Svgs from a single file

---
+++ public/index.ts
@@ -0,0 +1,8 @@
+export { default as two } from './2.png';
+export { default as three } from './3.png';
+export { default as two3 } from './23.svg';
+export { default as earthDark } from './earth-dark.jpg';
+export { default as footerGalaxy } from './footer-galaxy.webp';
+export { default as heroBg } from './hero-bg.png';
+export { default as heroDrone } from './hero-drone.png';
+export { default as pic } from './pic.png';
