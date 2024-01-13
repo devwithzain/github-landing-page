d3d824a658fedcdd677b9caee40e3fc5966d9787
Zain Ali <ZainSoftwear11@gmail.com>
2024-01-13 23:15:26 +0500

    Exported different components constants

---
+++ constants/index.ts
@@ -0,0 +1,486 @@
+import { security, packages, actions, codeSpace, copilot, codeReview, discussion, issues, share, twitter, facebook, linkedin, youtube, twitch, tiktok, GithubFooter, logo, kpmg, sap, telsus, png, marcedeeze, homeBrew, directus, dayhesoe, imolorhe, commandPost, kazupon, prophen, sindresorhus, eslint } from "@public";
+// Product Nav Constants
+export const productNavItem = [
+    {
+        id: 1,
+        title: "Action",
+        subTitle: "Automate any workflow",
+        icon: actions
+    },
+    {
+        id: 2,
+        title: "Packages",
+        subTitle: "Host and manage packages",
+        icon: packages
+    },
+    {
+        id: 3,
+        title: "Security",
+        subTitle: "Find and fix vulnerability",
+        icon: security,
+    },
+    {
+        id: 4,
+        title: "CodeSpace",
+        subTitle: "Instant dev environments",
+        icon: codeSpace
+    },
+    {
+        id: 5,
+        title: "Copilot",
+        subTitle: "Write better code with AI",
+        icon: copilot,
+    },
+    {
+        id: 6,
+        title: "Code review",
+        subTitle: "Manage code changes",
+        icon: codeReview,
+    },
+    {
+        id: 7,
+        title: "Issues",
+        subTitle: "Plan and track issues",
+        icon: issues,
+    },
+    {
+        id: 8,
+        title: "Discussions",
+        subTitle: "Collaborate outside of code",
+        icon: discussion,
+    },
+];
+
+//  ProductNav2 Constants
+export const productNavItem2 = [
+    {
+        id: 1,
+        title: "Documentaion",
+        icon: share
+    },
+    {
+        id: 2,
+        title: "GitHub skills",
+        icon: share,
+    },
+    {
+        id: 3,
+        title: "Blog",
+        icon: share
+    },
+];
+
+//  Solution Nav Constants
+export const solutionNavItem = [
+    {
+        id: 1,
+        title: "For",
+        links: [
+            {
+                id: 1,
+                title: "Enterprise",
+            },
+            {
+                id: 2,
+                title: "Teams",
+            },
+            {
+                id: 3,
+                title: "Startups",
+            },
+        ],
+    },
+];
+
+export const solutionNavItem1 = [
+    {
+        id: 1,
+        title: "By Solution",
+        links: [
+            {
+                id: 1,
+                title: "DevOps",
+                icon: share
+            },
+            {
+                id: 2,
+                title: "DevSecOps",
+                icon: share
+            },
+        ],
+    },
+];
+
+export const solutionNavItem2 = [
+    {
+        id: 1,
+        title: "Case Studies",
+        links: [
+            {
+                id: 1,
+                title: "Resources",
+                icon: share
+            },
+        ],
+    },
+];
+
+
+export const openSourceNavItem = [
+    {
+        id: 1,
+        title: "GitHub Sponsers",
+        subTitle: "Fund open source developers",
+    },
+    {
+        id: 2,
+        title: "The ReadMe Projects",
+        subTitle: "Fund open source developers",
+    },
+];
+
+export const openSourceNavItem1 = [
+    {
+        id: 1,
+        title: "Repositories",
+        links: [
+            {
+                id: 1,
+                title: "Topics",
+            },
+            {
+                id: 2,
+                title: "Trendings",
+            },
+            {
+                id: 3,
+                title: "Collections",
+            },
+        ]
+    }
+];
+
+// Hero Constants
+export const heroBrandsItem = [
+    { id: 1, src: kpmg },
+    { id: 2, src: sap },
+    { id: 3, src: telsus },
+    { id: 4, src: png },
+    { id: 5, src: marcedeeze },
+];
+
+// Collaboration Items
+
+export const colabItems = [
+    {
+        id: 1,
+        title: "Homebrew",
+        subTitle: "Sponsor",
+        src: homeBrew
+    },
+    {
+        id: 2,
+        title: "Directus",
+        subTitle: "Sponsor",
+        src: directus
+    },
+    {
+        id: 3,
+        title: "Nick DeJesus",
+        subTitle: "Sponsor",
+        src: dayhesoe
+    },
+    {
+        id: 4,
+        title: "Samuel",
+        subTitle: "Sponsor",
+        src: imolorhe
+    },
+    {
+        id: 5,
+        title: "CommandPost",
+        subTitle: "Sponsor",
+        src: commandPost
+    },
+    {
+        id: 6,
+        title: "kazuya",
+        subTitle: "Sponsor",
+        src: kazupon
+    },
+    {
+        id: 7,
+        title: "Nikema",
+        subTitle: "Sponsor",
+        src: sindresorhus
+    },
+    {
+        id: 8,
+        title: "Sindresorhus",
+        subTitle: "Sponsor",
+        src: homeBrew
+    },
+    {
+        id: 9,
+        title: "ESLint",
+        subTitle: "Sponsor",
+        src: eslint
+    },
+];
+
+// Footer Constants
+export const footerLinksItem = [
+    {
+        id: 1,
+        title: "Product",
+        links: [
+            {
+                id: 1,
+                title: "Features",
+                href: "Features",
+            },
+            {
+                id: 2,
+                title: "Security",
+                href: "Security",
+            },
+            {
+                id: 3,
+                title: "Team",
+                href: "Team",
+            },
+            {
+                id: 4,
+                title: "Enterprise",
+                href: "Enterprise",
+            },
+            {
+                id: 5,
+                title: "Custom Stories",
+                href: "Custom Stories",
+            },
+            {
+                id: 6,
+                title: "The ReadME Project",
+                href: "The ReadME Project",
+            },
+            {
+                id: 7,
+                title: "Pricing",
+                href: "Pricing",
+            },
+            {
+                id: 8,
+                title: "Resources",
+                href: "Resources",
+            },
+            {
+                id: 9,
+                title: "Roadmap",
+                href: "Roadmap",
+            },
+            {
+                id: 10,
+                title: "Compare GitHub",
+                href: "Compare GitHub",
+            },
+        ]
+    },
+    {
+        id: 2,
+        title: "Platform",
+        links: [
+            {
+                id: 1,
+                title: "Developer API",
+                href: "Developer API",
+            },
+            {
+                id: 2,
+                title: "Partners",
+                href: "Partners",
+            },
+            {
+                id: 3,
+                title: "Electron",
+                href: "Electron",
+            },
+            {
+                id: 4,
+                title: "GitHub Desktop",
+                href: "GitHub Desktop",
+            },
+        ]
+    },
+    {
+        id: 3,
+        title: "Product",
+        links: [
+            {
+                id: 1,
+                title: "Docs",
+                href: "Docs",
+            },
+            {
+                id: 2,
+                title: "Community Forum",
+                href: "Community Forum",
+            },
+            {
+                id: 3,
+                title: "Professional Services",
+                href: "Professional Services",
+            },
+            {
+                id: 4,
+                title: "Premium Support",
+                href: "Premium Support",
+            },
+            {
+                id: 5,
+                title: "Skill",
+                href: "Skill",
+            },
+            {
+                id: 6,
+                title: "Status",
+                href: "Status",
+            },
+            {
+                id: 7,
+                title: "Contact GitHub",
+                href: "Contact GitHub",
+            },
+        ]
+    },
+    {
+        id: 4,
+        title: "Company",
+        links: [
+            {
+                id: 1,
+                title: "About",
+                href: "About",
+            },
+            {
+                id: 2,
+                title: "Blog",
+                href: "Blog",
+            },
+            {
+                id: 3,
+                title: "Careers",
+                href: "Careers",
+            },
+            {
+                id: 4,
+                title: "Press",
+                href: "Press",
+            },
+            {
+                id: 5,
+                title: "Inclusion",
+                href: "Inclusion",
+            },
+            {
+                id: 6,
+                title: "Social Impact",
+                href: "Social Impact",
+            },
+            {
+                id: 7,
+                title: "Shop",
+                href: "Shop",
+            },
+            {
+                id: 8,
+                title: "Customers Stories",
+                href: "Customers Stories",
+            },
+            {
+                id: 9,
+                title: "The ReadMe Project",
+                href: "The ReadMe Project",
+            },
+        ]
+    }
+];
+
+export const footerSocialItems = [
+    {
+        id: 1,
+        href: "https://www.twitter.com",
+        src: twitter
+    },
+    {
+        id: 2,
+        href: "https://www.facebook.com",
+        src: facebook
+    },
+    {
+        id: 3,
+        href: "https://www.linkedin.com",
+        src: linkedin
+    },
+    {
+        id: 4,
+        href: "https://www.youtube.com",
+        src: youtube
+    },
+    {
+        id: 5,
+        href: "https://www.twitch.com",
+        src: twitch
+    },
+    {
+        id: 6,
+        href: "https://www.tiktok.com",
+        src: tiktok
+    },
+    {
+        id: 7,
+        href: "https://www.github.com",
+        src: logo
+    },
+];
+
+
+export const footerTermsItem = [
+    {
+        id: 1,
+        title: "© 2024 GitHub, Inc.",
+        links: [
+            {
+                id: 1,
+                title: "Terms",
+                href: "/"
+            },
+            {
+                id: 2,
+                title: "Privacy (Updated 08/2022)",
+                href: "/"
+            },
+            {
+                id: 3,
+                title: "Sitemap",
+                href: "/"
+            },
+            {
+                id: 4,
+                title: "What is Git?",
+                href: "/"
+            },
+            {
+                id: 5,
+                title: "Manage cookies",
+                href: "/"
+            },
+            {
+                id: 6,
+                title: "Do not share my personal information",
+                href: "/"
+            },
+        ]
+    }
+];
+
