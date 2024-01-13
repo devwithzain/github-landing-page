40be05e3fb5b1845863673e2864896e2a5cc819d
Zain Ali <ZainSoftwear11@gmail.com>
2024-01-13 23:15:44 +0500

    Exported different components images

--- public/index.ts
+++ public/index.ts
@@ -1,8 +1,62 @@
 export { default as two } from './2.png';
-export { default as three } from './3.png';
 export { default as two3 } from './23.svg';
 export { default as earthDark } from './earth-dark.jpg';
 export { default as footerGalaxy } from './footer-galaxy.webp';
 export { default as heroBg } from './hero-bg.png';
 export { default as heroDrone } from './hero-drone.png';
 export { default as pic } from './pic.png';
+export { default as logo } from './logo.svg';
+export { default as search } from './search.svg';
+export { default as dropdown } from './dropDownUn.svg';
+export { default as share } from './share.svg';
+export { default as security } from './security.svg';
+export { default as copilot } from './copilot.svg';
+export { default as packages } from './package.svg';
+export { default as codeSpace } from './codeSpace.svg';
+export { default as discussion } from './disscusion.svg';
+export { default as actions } from './actions.svg';
+export { default as codeReview } from './codeReview.svg';
+export { default as issues } from './issues.svg';
+
+// footer social
+export { default as GithubFooter } from './githubF.svg';
+export { default as twitter } from './twitter.svg';
+export { default as tiktok } from './tiktok.svg';
+export { default as twitch } from './twitch.svg';
+export { default as facebook } from './facebook.svg';
+export { default as youtube } from './youtube.svg';
+export { default as linkedin } from './linkEdin.svg';
+
+// hero brands
+export { default as kpmg } from './kpmg.svg';
+export { default as marcedeeze } from './marcedeze.svg';
+export { default as png } from './png.svg';
+export { default as sap } from './sap.svg';
+export { default as telsus } from './telsus.svg';
+
+// Productivity
+export { default as mobile } from './mobileProductivity.png';
+export { default as actionsPro } from './actionProductibity.png';
+
+// Collaboration
+export { default as issuesColab } from './issueColab.png';
+export { default as issueColab } from './issuesCollab.png';
+export { default as discussionColab } from './disscussion.png';
+export { default as pullColab } from './illu-pull-requests.webp';
+export { default as arcPink } from './arcPink.svg';
+export { default as bgStar } from './bg-stars.webp';
+
+export { default as homeBrew } from './homebrew.png';
+export { default as directus } from './directus.png';
+export { default as imolorhe } from './imolorhe.jpeg';
+export { default as dayhesoe } from './dayhaysoos.jpeg';
+export { default as commandPost } from './commandpost.png';
+export { default as kazupon } from './kazupon.webp';
+export { default as prophen } from './prophen.webp';
+export { default as sindresorhus } from './sindresorhus.webp';
+export { default as eslint } from './eslint.png';
+
+// Security
+export { default as CodeScaning } from './codeScaninigSec.png';
+export { default as dependbot } from './dependbotSecu.png';
+export { default as envScaning } from './scaningSec.png';
