cb6b14fcb286efbb06f3851965814d7102bd3b7d
Zain Ali <ZainSoftwear11@gmail.com>
2024-01-15 23:07:36 +0500

    Exported different components

--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,7 @@
 export { default as Home } from './Home';
 export { default as Navbar } from './Navbar/Navbar';
 export { default as Hero } from './Hero-section/Hero';
-// export { default as StickyNav } from './Navbar/StickyNav';
+export { default as StickyNav } from './Navbar/StickyNav';
 export { default as Security } from './Security/Security';
 export { default as Globe } from './Footer/Globe';
 export { default as Footer } from './Footer/Footer';
