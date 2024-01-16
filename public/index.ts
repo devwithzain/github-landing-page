d46edc8aa979bf2814d861b5d1c2f63254476777
Zain Ali <ZainSoftwear11@gmail.com>
2024-01-16 23:20:32 +0500

    Completed

--- public/index.ts
+++ public/index.ts
@@ -1,10 +1,4 @@
-export { default as two } from './2.png';
-export { default as two3 } from './23.svg';
-export { default as earthDark } from './earth-dark.jpg';
 export { default as footerGalaxy } from './footer-galaxy.webp';
-export { default as heroBg } from './hero-bg.png';
-export { default as heroDrone } from './hero-drone.png';
-export { default as pic } from './pic.png';
 export { default as logo } from './logo.svg';
 export { default as search } from './search.svg';
 export { default as dropdown } from './dropDownUn.svg';
@@ -14,12 +8,12 @@ export { default as copilot } from './copilot.svg';
 export { default as packages } from './package.svg';
 export { default as codeSpace } from './codeSpace.svg';
 export { default as discussion } from './disscusion.svg';
+export { default as dropDown } from './dropDown.png';
 export { default as actions } from './actions.svg';
 export { default as codeReview } from './codeReview.svg';
 export { default as issues } from './issues.svg';
 
 // footer social
-export { default as GithubFooter } from './githubF.svg';
 export { default as twitter } from './twitter.svg';
 export { default as tiktok } from './tiktok.svg';
 export { default as twitch } from './twitch.svg';
@@ -33,18 +27,28 @@ export { default as marcedeeze } from './marcedeze.svg';
 export { default as png } from './png.svg';
 export { default as sap } from './sap.svg';
 export { default as telsus } from './telsus.svg';
+export { default as threeM } from './3m.svg';
 
 // Productivity
 export { default as mobile } from './mobileProductivity.png';
 export { default as actionsPro } from './actionProductibity.png';
+export { default as proArc } from './proArc.svg';
+export { default as procodeSpace } from './proCodespace.png';
+export { default as proSideArc } from './proSideArc.svg';
+export { default as arcGreenPro } from './arcGreenPro.svg';
+export { default as proCover } from './prodCover.png';
+export { default as proCoverTop } from './prodCoverTop.png';
+export { default as proBag } from './bag.png';
 
 // Collaboration
 export { default as issuesColab } from './issueColab.png';
 export { default as issueColab } from './issuesCollab.png';
 export { default as discussionColab } from './disscussion.png';
-export { default as pullColab } from './illu-pull-requests.webp';
-export { default as arcPink } from './arcPink.svg';
+// export { default as pullColab } from './illu-pull-requests.webp';
+export { default as arcPink } from './arcPinkCollab.svg';
 export { default as bgStar } from './bg-stars.webp';
+export { default as collabArc } from './collabArc.svg';
+export { default as collabBracket } from './collabBracket.png';
 
 export { default as homeBrew } from './homebrew.png';
 export { default as directus } from './directus.png';
@@ -60,3 +64,6 @@ export { default as eslint } from './eslint.png';
 export { default as CodeScaning } from './codeScaninigSec.png';
 export { default as dependbot } from './dependbotSecu.png';
 export { default as envScaning } from './scaningSec.png';
+export { default as securityIll } from './securityIll.svg';
+export { default as arcGreen } from './arcGreen.svg';
+export { default as criticalSec } from './critical.png';
