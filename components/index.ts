19d910259caf2f59dd3cd57b21b8deb4abff9c73
devwithzain <zainsoftwear11@gmail.com>
2024-05-26 04:54:59 -0700

    modified

--- components/index.ts
+++ components/index.ts
@@ -3,7 +3,6 @@ export { default as Navbar } from './Navbar/Navbar';
 export { default as Hero } from './Hero-section/Hero';
 export { default as StickyNav } from './Navbar/StickyNav';
 export { default as Security } from './Security/Security';
-export { default as Globe } from './Footer/Globe';
 export { default as Footer } from './Footer/Footer';
 export { default as Productivity } from './Productivity/Productivity';
 export { default as Collaboration } from './Collaboration/Collaboration';
